5a8e0043bdbf16fe405b19d4fd1d8a86b6a4e75b
ARIEL NICOLLAS <39384032+arielnicollas@users.noreply.github.com>
2023-05-04 11:20:36 -0300

    feature: criando a conexão com o banco de dados e implementando o lógica para realizar uma requisição com o metodo GET

--- src/app.js
+++ src/app.js
@@ -1,20 +1,32 @@
 import express from "express";
+import db from "./config/dbconnect.js";
+import livros from "./models/Livro.js"
+
+db.on("erro", console.log.bind(console, 'Erro de conexão'))
+db.once("open", () => {
+    console.log("Conexão com banco feita com sucesso.")
+})
 
 const app = express()
 
 app.use(express.json())
 
-const livros = [
-    {id: 1, "titulo" : "Robbit"},
-    {id: 2, "titulo" : "Senhor dos Aneis"}
-]
+// const livros = [
+//     {id: 1, "titulo" : "Robbit"},
+//     {id: 2, "titulo" : "Senhor dos Aneis"}
+// ]
 
 app.get('/', (req,res) => {
     res.send("Sevidor ON")
 })
 
-app.get('/livros', (req,res) => {
-    res.status(200).send(livros)
+app.get('/livros', async (req,res) => {
+    try{
+        const livrosResultado = await livros.find();
+        res.status(200).json(livrosResultado)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
 
 app.get('/livros/:id', (req,res) => {
